323ac985eb641f2f5782a655b49eff762e2b502e
cary <carywill.k@gmail.com>
2021-11-23 14:23:03 +0800

    fix: lastModified

--- http-cache/app.js
+++ http-cache/app.js
@@ -15,21 +15,35 @@ let app = http.createServer((req, res) => {
   switch (extname) {
     case ".js":
       contentType = "text/javascript";
+      fs.writeFileSync(filePath, new Date().toISOString(), "utf-8");
       break;
   }
 
   fs.readFile(filePath, function (error, content) {
     // res.removeHeader("Cache-Control");
+    if (error) return;
     res.writeHead(200, {
       "Content-Type": contentType,
       // Expires: "Wed, 21 Oct 2022 07:28:00 GMT",
-      "Cache-Control": "public,max-age=10",
+      // "Cache-Control": "public,max-age=10",
     });
-    res.end(content, "utf-8");
+    let status = fs.statSync(filePath);
+    let lastModified = status.mtime.toUTCString();
+    if (lastModified === req.headers["if-modified-since"]) {
+      res.writeHead(304, "Not Modified");
+      res.end();
+    } else {
+      res.writeHead(200, "OK");
+      res.writeHead(200, {
+        "Cache-Control": "public,max-age=10",
+        "Last-Modified": lastModified,
+      });
+      res.end(content);
+    }
   });
 });
 
 // Start the server on port 3000
 app.listen(3000, "127.0.0.1");
-open("http://localhost:3000/");
+open("http://localhost:3000/index.html");
 console.log("Node server running on port 3000");
