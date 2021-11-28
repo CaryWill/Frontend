5b7aec8ebb14ae03490170aaa9777f9a1f92fdf8
cary <CaryWill.K@gmail.com>
2021-11-28 11:26:37 +0800

    fix: add leading

--- debounce/index.js
+++ debounce/index.js
@@ -1,9 +1,30 @@
 // usecase: resize/input
-const debounce = (fn, delay) => {
+// const debounce = (fn, delay) => {
+//   let timer;
+//   return function debounced(...args) {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => fn(...args), delay);
+//   };
+// };
+const debounce = (fn, delay = 2000, options = { leading: false }) => {
   let timer;
+  const { leading } = options;
   return function debounced(...args) {
+    console.log("test");
     clearTimeout(timer);
-    timer = setTimeout(() => fn(...args), delay);
+    if (leading && !timer) {
+      fn(...args);
+      timer = setTimeout(() => {
+        fn(...args);
+        timer = null;
+      }, delay);
+    } else {
+      // 调用过程中才会设置定时器
+      timer = setTimeout(() => {
+        fn(...args);
+        timer = null;
+      }, delay);
+    }
   };
 };
 
