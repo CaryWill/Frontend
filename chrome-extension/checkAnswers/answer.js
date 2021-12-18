6ba777ca46772b8be60f23ea67f589cc1d6577b7
cary <CaryWill.K@gmail.com>
2021-12-18 16:25:10 +0800

    fix: style

--- chrome-extension/checkAnswers/answer.js
+++ chrome-extension/checkAnswers/answer.js
@@ -1,9 +1,9 @@
 function checkAnswers(answers = "") {
-  //   const sujects = document.querySelectorAll(".subjectBox");
-  //   answers.split("").forEach((item, index) => {
-  //     sujects[index].querySelectorAll("label")[item - 1].click();
-  //   });
-  document.body.innerHTML = config;
+  const sujects = document.querySelectorAll(".subjectBox");
+  answers.split("").forEach((item, index) => {
+    sujects[index].querySelectorAll("label")[item - 1].click();
+  });
+  // document.body.innerHTML = config;
 }
 checkAnswers(config);
 // 44244424333332
