93364df1eeeb0a329c03b488e0c5ebd7f2b4f127
cary <CaryWill.K@gmail.com>
2021-12-19 12:40:45 +0800

    fix: update

--- chrome-extension/checkAnswers/answer.js
+++ chrome-extension/checkAnswers/answer.js
@@ -1,11 +1,30 @@
 function checkAnswers(answers = "") {
-  const sujects = document.querySelectorAll(".subjectBox");
-  answers.split("").forEach((item, index) => {
-    sujects[index].querySelectorAll("label")[item - 1].click();
-  });
-  // document.body.innerHTML = config;
+  if (
+    document.getElementById("nowFormTitle").textContent.trim().includes("PSQI")
+  ) {
+    // PSQI
+    const a = answers.slice();
+    const sujects = document.querySelectorAll(".subjectBox").map((s) => {
+      const firstLetter = s.firstElementChild.textContent.trim()[0];
+      if (Number(firstLetter) >= 5 && Number(firstLetter) <= 19) {
+        return true;
+      } else if (["a", "b", "c", "d", "e"].includes(firstLetter)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    a.split("").forEach((item, index) => {
+      sujects[index].querySelectorAll("label")[item - 1].click();
+    });
+  } else {
+    // 汉密尔顿抑郁量表（HAMD-17）
+    // 汉密尔顿焦虑量表（HAMA）
+    // PHQ-9
+    const sujects = document.querySelectorAll(".subjectBox");
+    answers.split("").forEach((item, index) => {
+      sujects[index].querySelectorAll("label")[item - 1].click();
+    });
+  }
 }
 checkAnswers(config);
-// 44244424333332
-// 汉密尔顿抑郁量表（HAMD-17）
-// 汉密尔顿焦虑量表（HAMA）
