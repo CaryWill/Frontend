b1d5c20f93ddfb7321b5c82eed16f9f4f4107f3a
cary <CaryWill.K@gmail.com>
2022-03-03 22:44:23 +0800

    fix: update

--- react-fiber/lib/index.js
+++ react-fiber/lib/index.js
@@ -1,46 +1,32 @@
-
-function createElement(type, props, ...children) {
-  return {
-    type,
-    props: { ...props,
-      children: children.map(child => typeof child === "object" ? child : createTextElement(child))
-    }
-  };
-}
-
-function createTextElement(text) {
-  return {
-    type: "TEXT_ELEMENT",
-    props: {
-      nodeValue: text,
-      children: []
-    }
-  };
-}
-
-function render(element, container) {
-  const dom = element.type == "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type);
-
-  const isProperty = key => key !== "children";
-
-  Object.keys(element.props).filter(isProperty).forEach(name => {
-    dom[name] = element.props[name];
-  });
-  element.props.children.forEach(child => render(child, dom));
-  container.appendChild(dom);
-}
-
-var Didact = {
-  createElement,
-  render
-}; // 告诉 babel 使用 Didact 的 createElement 来支持 jsx 语法 
-
-/** @jsx Didact.createElement */
-
-const element = Didact.createElement("div", {
-  style: "background: salmon"
-}, Didact.createElement("h1", null, "Hello World"), Didact.createElement("h2", {
-  style: "text-align:right"
-}, "from Didact"));
-const container = document.getElementById("root");
-Didact.render(element, container);
+// vnode:
+// {
+//   type: 'div',
+//   props: {
+//     children: [
+//       {
+//         type: 'h1',
+//         props: {} 
+//       },
+//       {
+//         type: 'h2',
+//         props: {} 
+//       }
+//     ]
+//   }
+// }
+// function render(vnode, container) {
+//   const {type, props: {children, nodeValue = '', ...rest}} = vnode;
+//   let node; // dom node
+//   if (type === 'TEXT_ELEMENT') {
+//     node = document.createTextNode(nodeValue);
+//   } else {
+//     node = document.createElement(type);
+//     children?.forEach((child) => {
+//       render(child, node);
+//     })
+//   }
+//   container.appendChild(node);
+// };
+const vnode = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "h1"), /*#__PURE__*/React.createElement("h2", null, "h2"));
+const container = document.getElementById('root');
+render(element, container);
