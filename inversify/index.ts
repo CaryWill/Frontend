6dc5d5909c671886b12fc79d08488ea6329dd6ff
cary <cary@MacBook-Pro.local>
2022-03-27 22:26:18 +0800

    fix: add js demo from github

--- inversify/index.ts
+++ inversify/index.ts
@@ -1,3 +1,6 @@
+// Step 1: Declare your interfaces and types
+// Our goal is to write code that adheres to the dependency inversion principle. This means that we should "depend upon Abstractions and do not depend upon concretions". Let's start by declaring some interfaces (abstractions).
+
 import { myContainer } from "./inversify.config";
 import { TYPES } from "./types";
 import { Warrior } from "./interfaces";
