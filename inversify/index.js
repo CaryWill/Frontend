40f2e01048de39eab95cecdec6e33d25749fab07
cary <cary@Macintosh.local>
2022-04-17 14:41:23 +0800

    fix: update

--- inversify/index.js
+++ inversify/index.js
@@ -1,51 +1,20 @@
 var inversify = require("inversify");
 require("reflect-metadata");
 
-var TYPES = {
-  Ninja: "Ninja",
-  Katana: "Katana",
-  Shuriken: "Shuriken",
-};
-
-class Katana {
-  hit() {
-    return "cut!";
-  }
-}
-
-class Shuriken {
-  throw() {
-    return "hit!";
-  }
-}
-
 class Ninja {
-  constructor(katana, shuriken) {
-    this._katana = katana;
-    this._shuriken = shuriken;
-  }
   fight() {
-    return this._katana.hit();
-  }
-  sneak() {
-    return this._shuriken.throw();
+    return "fight!"
   }
 }
 
-// Declare as injectable and its dependencies
-inversify.decorate(inversify.injectable(), Katana);
-inversify.decorate(inversify.injectable(), Shuriken);
 inversify.decorate(inversify.injectable(), Ninja);
-inversify.decorate(inversify.inject(TYPES.Katana), Ninja, 0);
-inversify.decorate(inversify.inject(TYPES.Shuriken), Ninja, 1);
+const container = new inversify.Container();
+// container.bind("Ninja").to(Ninja);
+container.bind("Ninja").toDynamicValue(() => { return Promise.resolve({fight: () => 'My fight!'}); });
+// console.log(container._bindingDictionary._map.entries());
+const fight = async () => {
+  const ninja = await container.getAsync("Ninja")
+  console.log(ninja.fight());
+}
 
-// Declare bindings
-var container = new inversify.Container();
-container.bind(TYPES.Ninja).to(Ninja);
-container.bind(TYPES.Katana).to(Katana);
-container.bind(TYPES.Shuriken).to(Shuriken);
-
-// Resolve dependencies
-var ninja = container.get(TYPES.Ninja);
-console.log(Reflect.getMetadata(Ninja, Ninja));
-// console.log(ninja.sneak());
+fight()
--- inversify/index.ts
+++ inversify/index.ts
@@ -1,10 +1,16 @@
-// Step 1: Declare your interfaces and types
-// Our goal is to write code that adheres to the dependency inversion principle. This means that we should "depend upon Abstractions and do not depend upon concretions". Let's start by declaring some interfaces (abstractions).
+var inversify = require("inversify");
+require("reflect-metadata");
 
-import { myContainer } from "./inversify.config";
-import { TYPES } from "./types";
-import { Warrior } from "./interfaces";
+class Ninja {
+  fight() {
+    return "fight!"
+  }
+}
 
-const ninja = myContainer.get<Warrior>(TYPES.Warrior);
-console.log(ninja.fight());
-console.log(ninja.sneak());
+inversify.decorate(inversify.injectable(), Ninja);
+const ninja_id = "Ninja"; 
+const container = new inversify.Container();
+// 用这个 ninja_id key（serviceIdentifier） 来 map 一个实现类
+container.bind(ninja_id).to(Ninja);
+// 分析依赖，注入依赖，初始化实例
+// const ninja = container.get(ninja_id);
