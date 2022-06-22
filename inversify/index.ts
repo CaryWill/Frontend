2dd5ab818ba7620a6d66049234232694337df23c
cary <carywill.k@gmail.com>
2022-06-22 20:16:35 +0800

    fix: bind.toSelf test

--- inversify/index.ts
+++ inversify/index.ts
@@ -23,8 +23,19 @@ class Shuriken {
 const myContainer = new Container();
 myContainer.bind("Weapon").to(Shuriken);
 myContainer.bind("Warrior").to(Ninja);
-myContainer.bind("Warrior").to(Ninja);
 //myContainer.bind(Symbol.for("Warrior")).to(Ninja);
 //myContainer.bind(Symbol.for("Warrior")).to(Ninja);
 //const ninja = myContainer.get<Ninja>("Warrior");
 //ninja.attack();
+
+// Toself
+@injectable()
+class Apple {}
+
+@injectable()
+class Orange {}
+
+myContainer.bind(Apple).toSelf().inSingletonScope();
+myContainer.bind(Orange).toSelf().inSingletonScope();
+console.log(myContainer.get(Apple))
+console.log(myContainer.get(Orange))
