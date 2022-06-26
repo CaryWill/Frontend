9cbdda756b6914b6d3a2132c4c0e25818312db30
cary <carywill.k@gmail.com>
2022-06-26 10:16:35 +0800

    fix: test to service will alse return an instance of the class we bound to

--- inversify/index.ts
+++ inversify/index.ts
@@ -37,16 +37,33 @@ class Orange {}
 
 myContainer.bind(Apple).toSelf().inSingletonScope();
 myContainer.bind(Orange).toSelf().inSingletonScope();
-console.log(myContainer.get(Apple));
-console.log(myContainer.get(Orange));
+//console.log(myContainer.get(Apple));
+//console.log(myContainer.get(Orange));
 
 // --- constraints ---
 // https://github.com/inversify/InversifyJS/blob/master/wiki/contextual_bindings.md
 myContainer.bind("cary").to(Ninja).whenTargetNamed("goodguy");
 myContainer.bind("cary").to(Ninja).whenTargetNamed("goodboy");
 
-console.log(myContainer.isBoundNamed("cary", "goodguy"));
-console.log(myContainer.isBoundNamed("cary", "goodboy"));
-console.log(myContainer.isBoundNamed("cary", "badboy"));
-console.log(myContainer.getNamed("cary", "goodguy"));
-console.log(myContainer.getAll());
+//console.log(myContainer.isBoundNamed("cary", "goodguy"));
+//console.log(myContainer.isBoundNamed("cary", "goodboy"));
+//console.log(myContainer.isBoundNamed("cary", "badboy"));
+//console.log(myContainer.getNamed("cary", "goodguy"));
+
+// -- service ---
+@injectable()
+class Juice {
+  name = "juice";
+}
+
+myContainer.bind(Juice).toSelf().inSingletonScope();
+myContainer.bind("juice").toService(Juice);
+myContainer.bind("juice1").toConstantValue(Juice);
+console.log(myContainer.get("juice"), myContainer.get("juice1"));
+
+@injectable()
+class Rocket {
+  name = "rocket"
+}
+myContainer.bind("rocket").to(Rocket);
+console.log((myContainer.get("rocket") as any).name);
