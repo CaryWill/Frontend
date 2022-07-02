4755075ac62b4ddb3c71346f491978858c368419
cary <carywill.k@gmail.com>
2022-07-02 16:07:30 +0800

    fix: update dist file

--- mini-sos/src/index.tsx
+++ mini-sos/src/index.tsx
@@ -11,17 +11,20 @@ sos.bootstrap();
 // 注册模块
 sos.container
   .get("ModuleService")
-  .registerModule("@cary/demo", "http://127.0.0.1:8080/index.39bcbeb6");
-const OtherComponent = React.lazy(() =>
-  sos.container.get("ModuleService").loadModule("@cary/demo")
-);
+  .registerModule(
+    "@cary/demo",
+    "https://cdn.jsdelivr.net/gh/CaryWill/Frontend/mini-sos/Demo/dist/assets/index.5b450732"
+  );
+
+const myModule = sos.container
+  .get("ModuleService")
+  .loadModule("@cary/demo")
+  .then((m) => console.log(m));
+//const OtherComponent = React.lazy(() =>
+//);
 
 export default function App() {
   useEffect(() => {}, []);
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <OtherComponent />
-    </Suspense>
-  );
+  return <Suspense fallback={<div>Loading...</div>}></Suspense>;
 }
