d1751fcfb220afe1f470cab68f77967e34f6ba75
cary <carywill.k@gmail.com>
2022-09-10 15:14:51 +0800

    refactor

--- mini-sos/step3/src/utils.ts
+++ mini-sos/step3/src/utils.ts
@@ -1,29 +1,22 @@
-export const resolveBundleURL = (bundleManifest: any) => {
-  const version = bundleManifest.version ? `${bundleManifest.version}/` : "";
-  const segments = (
-    bundleManifest.url +
-    version +
-    bundleManifest.modulePath
-  ).split(".");
-  // remove extension
-  segments.pop();
-  return segments.join(".");
+export const resolveBundleURL = (bundle: any) => {
+  const { version = "", url, modulePath } = bundle;
+  // remove file extension
+  const fullURL = `${url}${version ? version + "/" : ""}${modulePath}` as any;
+  const [_, URL] = fullURL.match(/(.*)[.](.*)/);
+  return URL;
 };
 
-export const resolveFullName = (serviceManifest) => {
-  const { bundleName, name, entryPoint } = serviceManifest;
+export const resolveFullName = (serviceManifest: any) => {
   // 一般 name 和 entryPoint 是同名的
+  const { bundleName, name, entryPoint } = serviceManifest;
   return `${bundleName}.${name || entryPoint}`;
 };
 
-export const resolveBundleInfo = (manifest) => {
+export const resolveBundleInfo = (manifest: any) => {
   // TODO: use symbol to store g_config
   const bundleList = globalThis.g_config?.bundle?.list || [];
   const matching = bundleList.find(
-    (bundle) => bundle.bundleName === manifest.bundleName
+    (bundle: any) => bundle.bundleName === manifest.bundleName
   );
-  if (!matching) {
-    throw new Error("bundle not found");
-  }
   return matching;
 };
